baf5b23edd640621171eb3b1a2af9eb683874f94
Max Steklyanov <steklyanov@gmail.com>
2020-02-04 12:39:31 +0300

    implement vector saving with celery and vector compatsion with plain view
    Took 1 hour 21 minutes

--- image_comparsion/image_comparsion/settings.py
+++ image_comparsion/image_comparsion/settings.py
@@ -119,6 +119,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# celery
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
